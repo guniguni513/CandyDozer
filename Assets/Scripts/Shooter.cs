57a6ebc22e6ff9d8ebf1109edf7ad82066588ba4
guniguni <guniguni513@gmail.com>
2024-03-18 13:17:10 +0900

    P211まで

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    const int MaxShotPower = 5;
+    const int RecoverySeconds = 3;
+
+    int shotPower = MaxShotPower;
+
     public GameObject[] candyPrefabs;
     public Transform candyParentTransform;
     public CandyManager candyManager;
@@ -13,13 +18,13 @@ public class Shooter : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))Shot();
+        if (Input.GetButtonDown("Fire1")) Shot();
     }
 
     //キャンディのプレハブからランダムに1つ選ぶ
     GameObject SampleCandy()
     {
-        int index = Random.Range(0,candyPrefabs.Length);
+        int index = Random.Range(0, candyPrefabs.Length);
         return candyPrefabs[index];
     }
 
@@ -27,12 +32,13 @@ public class Shooter : MonoBehaviour
     {
         //画面のサイズとInputの割合からキャンディ生成のポジションを計算
         float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
-        return transform.position + new Vector3(x,0,0);
+        return transform.position + new Vector3(x, 0, 0);
     }
     public void Shot()
     {
         //キャンディを生成できる条件外ならばShotしない
-        if(candyManager.GetCandyAmount() <= 0) return;
+        if (candyManager.GetCandyAmount() <= 0) return;
+        if (shotPower <= 0) return;
         //プレハブからCandyオブジェクトを生成
         GameObject candy = (GameObject)Instantiate(
             SampleCandy(),
@@ -45,10 +51,36 @@ public class Shooter : MonoBehaviour
 
         //CandyオブジェクトのRigidbodyを取得し力と回転を加える
         Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
-        candyRigidBody.AddForce(transform.forward*shotForce);
-        candyRigidBody.AddTorque(new Vector3(0,shotTrque,0));
+        candyRigidBody.AddForce(transform.forward * shotForce);
+        candyRigidBody.AddTorque(new Vector3(0, shotTrque, 0));
 
         //Candyのストックを消費
         candyManager.ConsumeCandy();
+        //ShotPowerを消費
+        ConsumePower();
+    }
+
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+
+        //ShotPowerの残数を+の数で表示
+        string label = "";
+        for (int i = 0; i < shotPower; i++) label = label + "+";
+
+        GUI.Label(new Rect(50, 65, 100, 30), label);
+    }
+    void ConsumePower()
+    {
+        //ShotPowerを消費すると同時に回復のカウントをスタート
+        shotPower--;
+        StartCoroutine(RecoverPower());
+    }
+
+    IEnumerator RecoverPower()
+    {
+        //一定秒数待った後にshotPowerを回復
+        yield return new WaitForSeconds(RecoverySeconds);
+        shotPower++;
     }
 }
